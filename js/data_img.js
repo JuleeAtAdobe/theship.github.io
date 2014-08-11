08c4b7b7939f48aa6df7510759e6e26f72958ebb
theship <juburdekin@gmail.com>
2014-08-10 22:16:52 -0700

    first draft of juleeburdekin.com

---
+++ js/data_img.js
@@ -0,0 +1,7 @@
+TweenMax.from('#box1', 6, {scale:.1, alpha:0, x:'-250px', y:'-250px'});
+
+TweenMax.from('#box2', 6, {scale:.25, alpha:0, y:'350px'});
+
+TweenMax.from('#box3', 6, {scale:.25, alpha:0, x:'350px', y:'-350px'});
+
+TweenMax.from('#DV', 8, {y:'-350px'});
